f857b151bd0b28bd9e8444fa2ffd6aab54845098
Prateek Chaurasia <prateek18801@gmail.com>
2024-02-25 15:58:24 +0530

    Setup multer on upload route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import express, { Router } from "express";
+import multer from "multer";
 import { postSheetProcess } from "../controllers/sheet";
 
+const upload = multer({ dest: "uploads/" });
 const router: Router = express.Router();
 
-router.post("/v1/sheet/process", postSheetProcess);
+router.post("/v1/sheet/process", upload.single("sheet"), postSheetProcess);
 
 export default router;
