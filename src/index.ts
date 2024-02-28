95af5d1ca7d6034b1d784f820c351bdcdb6f85c8
Prateek Chaurasia <prateek18801@gmail.com>
2024-02-28 22:41:21 +0530

    Refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,22 @@
-import express, { Express, json, urlencoded } from "express";
-import { Request, Response, NextFunction} from "express";
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import userUploadRoute from './router/index';
-//import db from "./utils/db";
+import express, { Express, Response, json, urlencoded } from "express";
+import cors from "cors";
+import router from "./router/index";
+import db from "./utils/db";
 
 const app: Express = express();
-dotenv.config();
-
-mongoose.set('strictQuery', true);
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
-app.use(cookieParser());
 
-app.use('/', userUploadRoute);
+app.use("/", router);
 
-app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
-    console.error(err);
-    const errorStatus = (err as any).status || 500;
-    const errorMessage = err.message || "Something went wrong!";
-    return res.status(errorStatus).send(errorMessage);
-  });
+app.get("/ping", (_req, res: Response) => {
+    return res.status(200).json({
+        message: "✅ server running"
+    });
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`✅ server started at http://localhost:${process.env.PORT}`);
-//   db.connect();
+    console.log("✅ server started");
+    db.connect();
 });
